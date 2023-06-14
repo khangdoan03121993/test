2d8613970475a48a5fd8ddb069fa6e8034b3dae8
anhngongoc <86513595+anhngongoc@users.noreply.github.com>
2023-06-14 16:38:28 +0700

    Update DemospringbootApplication.java

--- src/main/java/com/example/demospringboot/DemospringbootApplication.java
+++ src/main/java/com/example/demospringboot/DemospringbootApplication.java
@@ -3,6 +3,7 @@ package com.example.demospringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,6 +13,10 @@ public class DemospringbootApplication {
 	@GetMapping("/demo")
 	public String demo(){
 		return "demo by anhnn";
+	}
+	@GetMapping("/{input}")
+	public String data(@PathVariable("input") String input){
+		return "hi " +input + " application";
 	}
 
 	public static void main(String[] args) {
