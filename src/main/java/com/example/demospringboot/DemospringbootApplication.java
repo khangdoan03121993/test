5d265654d6aa774af6787ab502b688d681b19029
anhngongoc <86513595+anhngongoc@users.noreply.github.com>
2023-06-14 16:03:39 +0700

    Add files via upload

---
+++ src/main/java/com/example/demospringboot/DemospringbootApplication.java
@@ -0,0 +1,21 @@
+package com.example.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemospringbootApplication {
+
+	@GetMapping("/demo")
+	public String demo(){
+		return "demo by anhnn";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringbootApplication.class, args);
+	}
+
+}
